5e5222d150e1a8d9ed5b14e4f1a246f84bb73f63
heetaelee <josepheelee@gmail.com>
2023-12-22 00:43:20 +0900

    푸터 프론트 엔드 완성

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,21 +1,13 @@
-import React, {useState} from 'react';
-import BoardItem from 'components/BoardItem';
-import latestBoardListMock from "./mocks/latest-baord-list.mock";
-import Top3Item from "./components/Top3Item";
-import {commentListMock, favoriteListMock, top3BoardListMock} from "./mocks";
-import CommentItem from "./components/CommentItem";
 import './App.css'
-import FavoriteItem from "./components/FavoriteItem";
-import InputBox from "./components/InputBox";
+import Footer from 'layouts/Footer'
+
 
 
 function App() {
 
-    const [value, setValue] = useState<string>('')
-
   return (
     <>
-        <InputBox label='이메일' type='text' placeholder='이메일 주소를 입력해주세요' value={value} error={true} setValue={setValue} message=''/>
+        <Footer />
 
 </>
 
