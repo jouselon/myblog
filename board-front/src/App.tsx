dd57752cd544b6cc49c90e928f514051401c840c
heetaelee <josepheelee@gmail.com>
2023-12-21 23:27:58 +0900

    회원가입 프론트엔드 진행 중

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import BoardItem from 'components/BoardItem';
 import latestBoardListMock from "./mocks/latest-baord-list.mock";
 import Top3Item from "./components/Top3Item";
@@ -6,14 +6,16 @@ import {commentListMock, favoriteListMock, top3BoardListMock} from "./mocks";
 import CommentItem from "./components/CommentItem";
 import './App.css'
 import FavoriteItem from "./components/FavoriteItem";
+import InputBox from "./components/InputBox";
 
 
 function App() {
+
+    const [value, setValue] = useState<string>('')
+
   return (
     <>
-        <div style={{display: 'flex', rowGap: '20px', columnGap: '30px' }}>
-            {favoriteListMock.map(favoriteListItem => <FavoriteItem favoriteListItem={favoriteListItem} />) }
-        </div>
+        <InputBox label='이메일' type='text' placeholder='이메일 주소를 입력해주세요' value={value} error={true} setValue={setValue} message=''/>
 
 </>
 
---
+++ board-front/src/components/InputBox/index.tsx
@@ -0,0 +1,62 @@
+import {forwardRef, Dispatch, SetStateAction, ChangeEvent, KeyboardEvent} from 'react';
+import './style.css';
+import {Simulate} from "react-dom/test-utils";
+import error = Simulate.error;
+import * as events from "events";
+
+//interface : Input Box 컴포넌트 Properties
+interface Props {
+    label : string;
+    type : 'text' | 'password';
+    error : boolean;
+    placeholder : string;
+    value : string;
+    setValue : Dispatch<SetStateAction<string>>
+
+    icon? : string;
+    onButtonClick?: () => void;
+
+    message?: string;
+
+    onKeyDown?: (event: KeyboardEvent<HTMLInputElement>) => void;
+}
+
+//component : Input Box 컴포넌트
+const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref) => {
+
+    //state: properties
+    const {label, type,placeholder,error,value,icon,message } =props;
+    const {setValue,onButtonClick,onKeyDown} = props;
+
+    //event handler : input 값 변경 이벤트 처리 함수
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const {value} = event.target;
+        setValue(value);
+    }
+
+    //event Handler : input 키 이벤트 처리 함수
+    const onKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+        if(!onKeyDown) return;
+        onKeyDown(event);
+    }
+
+    //render : Input Box 컴포넌트
+    return (
+        <div className='inputbox'>
+            <div className='inputbox-label'>{label}</div>
+            <div className={error ? 'inputbox-container-error' : 'inputbox-container'}>
+                <input ref={ref} type={type} className='input' placeholder={placeholder} value={value} onChange={onChangeHandler}/>
+                {onButtonClick !== undefined &&
+
+                <div className='icon-button'>
+                    {icon !== undefined && <div className={`icon ${icon}`}></div>}
+                </div>
+                }
+            </div>
+            {message !== undefined && (<div className='inputbox-message'>{message}</div>)}
+        </div>
+    )
+
+});
+
+export default InputBox;
